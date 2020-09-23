bdbf96779a8e0d8322dcb9974bb522c5b151b5a6
Abinash Panda <abinashp@protonmail.ch>
2020-09-24 00:00:16 +0530

    redirected updated

--- controller/redirect-tls.go
+++ controller/redirect-tls.go
@@ -6,5 +6,5 @@ import (
 
 // RedirectTLS to port 443
 func RedirectTLS(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, ":443/"+r.RequestURI, http.StatusMovedPermanently)
+	http.Redirect(w, r, "https://abinash.tech:443/"+r.RequestURI, http.StatusMovedPermanently)
 }
